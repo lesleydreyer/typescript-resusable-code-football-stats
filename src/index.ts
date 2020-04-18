0a47379f3301bffbfa9cd83006a03311a7312a69
Lelsey Dreyer <lesleydreyer@hotmail.com>
2020-04-18 16:55:13 -0700

    sections 102-104 enums

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,25 @@ const matches = fs
 // H for home A for away team that won index 5
 
 // ANALYZE data
+// enums are basically the same as an object
+// but are good for signally to other developers
+// that this is a collection of closely related values
+// good for small sets of collections that won't change
+// like small|medium|large, primary colors, etc
+// but not things like blog post titles and netflix
+// movie categories that will change a lot
+enum MatchResult {
+	HomeWin = 'H',
+	AwayWin = 'A',
+	Draw = 'D',
+}
 let manUnitedWins = 0;
+
 for (let match of matches) {
-	if (match[1] === 'Man United' && match[5] === 'H') {
+	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
 		// manUnited won as home team
 		manUnitedWins++;
-	} else if (match[2] === 'Man United' && match[5] === 'A') {
+	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
 		// manUnited won as away team
 		manUnitedWins++;
 	}
